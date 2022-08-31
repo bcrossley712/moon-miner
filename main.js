40296f87744222a14908f1bcad6226594fd4edba
Brian Crossley <bcrossley712@gmail.com>
2022-08-31 15:12:22 -0600

    dictionary start

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 let cheese = 0
+let clickUpgrades = {
+  knife: {
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  },
+  drill: {
+    price: 250,
+    quantity: 0,
+    multiplier: 5
+  }
+};
+
+let automaticUpgrades = {
+  mousetronaut: {
+    price: 600,
+    quantity: 0,
+    multiplier: 20
+  },
+  station: {
+    price: 600,
+    quantity: 0,
+    multiplier: 20
+  }
+};
 function mine() {
   cheese++
   console.log(cheese);
