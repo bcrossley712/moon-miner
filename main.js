2c64ca01ebc326d77fa09f7d16ad06e273836b45
Brian Crossley <bcrossley712@gmail.com>
2022-09-01 16:36:28 -0600

    done for today

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
 let cheese = 0
 let clickUpgrades = {
   knife: {
-    price: 100,
+    price: 30,
     quantity: 0,
     multiplier: 1
   },
   drill: {
     price: 250,
     quantity: 0,
-    multiplier: 5
+    multiplier: 15
   }
 };
 
 let automaticUpgrades = {
   mousetronaut: {
-    price: 600,
+    price: 400,
     quantity: 0,
     multiplier: 20
   },
   station: {
-    price: 600,
+    price: 1500,
     quantity: 0,
-    multiplier: 20
+    multiplier: 200
   }
 };
 function mine() {
   cheese++
-  console.log(cheese);
+  for (const key in clickUpgrades) {
+    let upgrade = clickUpgrades[key]
+    cheese += upgrade.quantity * upgrade.multiplier
+  }
   document.getElementById("cheese-count").innerText = cheese
 }
 function runAutoUpgrade() {
@@ -39,15 +42,30 @@ function runAutoUpgrade() {
 function draw() {
   document.getElementById("cheese-count").innerText = cheese
   for (let key in clickUpgrades) {
-    // document.getElementById(key + "-count").innerHTML = automaticUpgrades[key].quantity
+    // document.getElementById(key + "-count").innerHTML = clickUpgrades[key].quantity
     document.getElementById(key).innerText = clickUpgrades[key].price
   }
+  for (let key in automaticUpgrades) {
+    // document.getElementById(key + "-count").innerHTML = automaticUpgrades[key].quantity
+    document.getElementById(key).innerText = automaticUpgrades[key].price
+  }
+
 }
 function upgradeClick(key) {
   let upgrade = clickUpgrades[key]
   cheese -= upgrade.price
   upgrade.quantity++
+  upgrade.price += (Math.floor(upgrade.price - upgrade.price * .9))
   console.log("Upgraded " + key);
+  draw()
+}
+function upgradeAuto(key) {
+  let upgrade = automaticUpgrades[key]
+  cheese -= upgrade.price
+  upgrade.quantity++
+  upgrade.price += (Math.floor(upgrade.price - upgrade.price * .7))
+  console.log("Upgraded " + key);
+  draw()
 }
 
 setInterval(runAutoUpgrade, 2000)
