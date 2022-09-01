6d68fc7c4b3140abee13904f8fbc9bef21649e79
Brian Crossley <bcrossley712@gmail.com>
2022-09-01 14:38:50 -0600

    working it

--- main.js
+++ main.js
@@ -29,3 +29,26 @@ function mine() {
   console.log(cheese);
   document.getElementById("cheese-count").innerText = cheese
 }
+function runAutoUpgrade() {
+  for (let key in automaticUpgrades) {
+    let upgrade = automaticUpgrades[key]
+    cheese += (upgrade.quantity * upgrade.multiplier)
+  }
+  draw()
+}
+function draw() {
+  document.getElementById("cheese-count").innerText = cheese
+  for (let key in clickUpgrades) {
+    // document.getElementById(key + "-count").innerHTML = automaticUpgrades[key].quantity
+    document.getElementById(key).innerText = clickUpgrades[key].price
+  }
+}
+function upgradeClick(key) {
+  let upgrade = clickUpgrades[key]
+  cheese -= upgrade.price
+  upgrade.quantity++
+  console.log("Upgraded " + key);
+}
+
+setInterval(runAutoUpgrade, 2000)
+draw()
